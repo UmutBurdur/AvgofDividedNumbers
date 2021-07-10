bbffc0f0b562d12d56e7acfaa661f20e109cdc22
Umut Burdur <euburdur28@gmail.com>
2021-07-11 00:51:40 +0300

    Changes applied

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,21 +7,22 @@ public class Main {
         int inputNumber = input.nextInt();
         int counter = 0;
         int total = 0;
-        float avg;
+        int avg;
 
         for (int i = 0; i <= inputNumber; i++) {
             if (i % 3 == 0 & i % 4 == 0) {
                 counter++;
                 total += i;
-
-
             }
-
-            if (i == inputNumber) {
+            if (i == inputNumber & total!=0) {
+                counter--;
                 avg = total / counter;
                 System.out.println("The average of the numbers that can be divided by 3 and 4 is "
                         + avg);
             }
+        }
+        if (total == 0){
+            System.out.println("There is no number that can be divided by 3 and 4");
         }
     }
 }
